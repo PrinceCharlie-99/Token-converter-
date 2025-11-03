9c35bcb7ba57b941ea93de9aa3cb4a0b8ff367be
PrinceCharlie-99 <omanjackie_20@yahoo.com>
2025-11-03 17:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, render_template_string
 from urllib.parse import unquote
 import json
+import jwt
+import datetime
 
 app = Flask(__name__)
+
+SECRET_KEY = "your_secret_key_here"  # इसे strong और secret रखें
 
 HTML_FORM = '''
 <!doctype html>
@@ -17,6 +21,8 @@ HTML_FORM = '''
   {% if token %}
     <h3>Converted Token (JSON):</h3>
     <pre>{{ token }}</pre>
+    <h3>JWT Token:</h3>
+    <pre>{{ jwt_token }}</pre>
   {% endif %}
 </body>
 </html>
@@ -29,16 +35,24 @@ def cookie_to_token(cookie_str):
         if '=' in cookie:
             key, value = cookie.split('=', 1)
             token_dict[key] = unquote(value)
-    return json.dumps(token_dict, indent=4)
+    return token_dict  # अब JSON नहीं, dict return करें
+
+def generate_jwt(payload, secret=SECRET_KEY):
+    payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    token = jwt.encode(payload, secret, algorithm="HS256")
+    return token
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     token = None
+    jwt_token = None
     if request.method == 'POST':
         cookie_str = request.form.get('cookie')
         if cookie_str:
-            token = cookie_to_token(cookie_str)
-    return render_template_string(HTML_FORM, token=token)
+            token_dict = cookie_to_token(cookie_str)
+            token = json.dumps(token_dict, indent=4)
+            jwt_token = generate_jwt(token_dict)
+    return render_template_string(HTML_FORM, token=token, jwt_token=jwt_token)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
